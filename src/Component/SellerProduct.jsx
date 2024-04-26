f8bac81478d5908fb296351fbcd9f22474b568eb
Mohamed Tamer <mtamr7854@gmail.com>
2024-04-26 04:33:47 +0300

    Complete Promote Page Task

--- src/Component/SellerProduct.jsx
+++ src/Component/SellerProduct.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import ProductPop from './ProductPop'
 import { FaCircleXmark } from 'react-icons/fa6'
-import { Link } from 'react-router-dom'
+
 const products = [
   {
     price: '100L.E',
@@ -22,12 +22,67 @@ const products = [
     capacity: 10,
   },
 ]
-
+const selectPromote = [
+  {
+    id: 'Sta',
+    price: 500,
+    label: 'Standard Ad 500EGP',
+  },
+  {
+    id: 'Pro',
+    price: 750,
+    label: 'Pro Ad 750EGP',
+  },
+  {
+    id: 'Pre',
+    price: 1000,
+    label: 'Premium Ad 1000EGP',
+  },
+]
+const PayPromote = [
+  {
+    name: 'credit',
+    placeholder: 'XXXX XXXX XXXX XXXX',
+    label: 'Enter Your Credit Card Number',
+    regex: /^\d{4}(?:\s\d{4}){3}$/,
+    error: 'Credit Card Number',
+  },
+  {
+    name: 'cvc',
+    placeholder: 'CVC/CVV',
+    label: 'CVC',
+    regex: /^\d{3}$/,
+    error: 'CVC Number',
+  },
+  {
+    name: 'ex',
+    placeholder: 'MM/YY',
+    label: 'Expire Date',
+    regex: /^(0[1-9]|1[0-2])\/(0[0-9]|1[0-9]|2[0-9]|3[0-9])$/,
+    error: 'Expire Date',
+  },
+  {
+    name: 'name',
+    placeholder: 'Name Of Credit Card',
+    label: 'Name',
+    regex: /^[A-Za-z]+(?: [A-Za-z]+)*$/,
+    error: 'Name',
+  },
+]
 function SellerProductPage() {
   const [show, setShow] = useState(false)
   const [showPromote, setShowPromote] = useState(false)
   const [proName, setProName] = useState('')
   const [AdPrice, setAdPrice] = useState(0)
+  const [pay, setPay] = useState(false)
+  const [error, setError] = useState('')
+  const [data, setData] = useState({
+    credit: '',
+    cvc: '',
+    ex: '',
+    name: '',
+  })
+
   function showPop() {
     if (show === true) {
       return <ProductPop />
@@ -94,7 +149,7 @@ function SellerProductPage() {
         <div
           className={`${
             showPromote ? '' : 'hidden'
-          } p-[15px] absolute rounded-lg w-[50%] h-fit bg-[white] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}
+          } p-[15px] absolute border border-[black] border-solid-[1px] rounded-lg w-[50%] h-fit bg-[white] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]`}
         >
           <FaCircleXmark
             onClick={() => {
@@ -106,42 +161,23 @@ function SellerProductPage() {
             <h1 className="font-bold mb-5">
               Select your promote for {proName}
             </h1>
-            <div className="p-[5px] mb-5">
-              <input
-                type="radio"
-                name="Promote"
-                id="Sta"
-                className="mr-2"
-                onClick={() => {
-                  setAdPrice(500)
-                }}
-              />
-              <label htmlFor="Sta">Standard Ad 500EGP</label>
-            </div>
-            <div className="p-[5px] mb-5">
-              <input
-                type="radio"
-                name="Promote"
-                id="Pro"
-                className="mr-2"
-                onClick={() => {
-                  setAdPrice(750)
-                }}
-              />
-              <label htmlFor="Pro">Pro Ad 750EGP</label>
-            </div>
-            <div className="p-[5px]">
-              <input
-                type="radio"
-                name="Promote"
-                id="Pre"
-                className="mr-2"
-                onClick={() => {
-                  setAdPrice(1000)
-                }}
-              />
-              <label htmlFor="Pre">Premium Ad 1000EGP</label>
-            </div>
+            {selectPromote.map((obj, index) => {
+              return (
+                <div className="p-[5px] mb-5" key={index}>
+                  <input
+                    type="radio"
+                    name="Promote"
+                    id={obj.id}
+                    required
+                    className="mr-2"
+                    onClick={() => {
+                      setAdPrice(obj.price)
+                    }}
+                  />
+                  <label htmlFor={obj.id}>{obj.label}</label>
+                </div>
+              )
+            })}
             {AdPrice ? (
               ''
             ) : (
@@ -149,13 +185,90 @@ function SellerProductPage() {
                 You must choose your promote
               </div>
             )}
-            <Link to={`${AdPrice ? '' : ''}`}>
-              <button className="block bg-[#3E64DA] text-center text-[white] w-[100%] rounded-lg">
-                Confirm
-              </button>
-            </Link>
+            <button
+              onClick={(e) => {
+                e.preventDefault()
+                if (AdPrice) {
+                  setPay(true)
+                  setShowPromote(false)
+                }
+              }}
+              className="block bg-[#3E64DA] text-center text-[white] w-[100%] rounded-lg"
+            >
+              Confirm
+            </button>
           </form>
         </div>
+        {pay ? (
+          <div className="border border-[black] border-solid-[1px] p-[15px] absolute rounded-lg w-[50%] h-fit bg-[white] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <FaCircleXmark
+              onClick={() => {
+                setPay(false)
+              }}
+              className="absolute right-[15px] top-[15px] cursor-pointer"
+            />
+            <form action="">
+              {PayPromote.map((obj, index) => {
+                return (
+                  <div key={index} className="my-3">
+                    <label htmlFor="credit" className="font-bold">
+                      {obj.label}
+                    </label>
+                    <input
+                      type="text"
+                      name={obj.name}
+                      id={obj.name}
+                      onFocus={(e) => {
+                        e.target.placeholder = ''
+                      }}
+                      onBlur={(e) => {
+                        if (!e.target.value) {
+                          e.target.placeholder = obj.placeholder
+                        }
+                      }}
+                      onChange={(e) => {
+                        const value = e.target.value
+                        const check = obj.regex
+                        const name = e.target.name
+                        // Example regex for a simple email validation
+                        const isValid = check.test(value)
+                        setError(isValid ? '' : obj.error)
+                        setData((prev) => {
+                          return { ...prev, [name]: value }
+                        })
+                        if (!e.target.value) {
+                          setError('')
+                        }
+                      }}
+                      required
+                      placeholder={obj.placeholder}
+                      className="w-full rounded-lg outline-none border border-[#3E64DA] border-solid-[1px] p-[5px] text-[13px]"
+                    />
+                    {error === obj.error ? (
+                      <div className="text-[13px] p-[5px] text-[red]">
+                        Invalid {obj.error}
+                      </div>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                )
+              })}
+              <button
+                type="submit"
+                onClick={() => {
+                  setPay(false)
+                  console.log(data)
+                }}
+                className="block w-full bg-[#3E64DA] p-[5px] text-white rounded-lg"
+              >
+                Complete Check
+              </button>
+            </form>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
     </main>
   )
