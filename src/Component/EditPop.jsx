1e4b41b4c64aefc3bb2c390a1a1897cbc3a65c2a
Mohamed Tamer <mtamr7854@gmail.com>
2024-06-22 02:16:06 +0300

    mo Edit Popup task

--- src/Component/EditPop.jsx
+++ src/Component/EditPop.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { FaCircleXmark } from 'react-icons/fa6'
+import OptionBox from './OptionBox'
 
 const editObj = {
   name: '',
@@ -16,13 +17,13 @@ function EditPop() {
         onClick={() => setPop(false)}
         className="absolute w-full min-h-screen top-0 left-0 bg-black opacity-50"
       ></div>
-      <div className="absolute text-[20px] rounded-[10px]  bg-white z-100  left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5">
+      <div className="absolute text-[15px] rounded-[10px]  bg-white z-100  left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5">
         <FaCircleXmark
           onClick={() => setPop(false)}
           className="absolute top-2 right-2 cursor-pointer text-[#3E64DA]"
         />
         <p className="text-[#3E64DA] p-2 text-center">Edit Page</p>
-        <form action="" className="text-[15px]">
+        {/* <form action="" className="text-[15px]">
           <label htmlFor="name">Name</label>
           <input
             type="text"
@@ -65,7 +66,23 @@ function EditPop() {
               editObj.photo = e.target.value
             }}
           />
-        </form>
+        </form> */}
+        <OptionBox title="Name" type="text" placeholder="Enter New Name" />
+        <OptionBox title="Price" type="number" placeholder="00.0 L.E" />
+        <OptionBox
+          title="Quantity"
+          type="number"
+          placeholder="Enter New Quantity"
+        />
+        <OptionBox
+          title="Description"
+          placeholder="Enter New Description at least 10 character"
+        />
+        <OptionBox
+          title="Choose Images (at least 1 image)"
+          type="file"
+          map="map"
+        />
         <div className="flex justify-around align-middle py-2 text-[15px] text-[#3E64DA]">
           <button
             onClick={() => {
